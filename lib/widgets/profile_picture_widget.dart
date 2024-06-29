c0231604955544a633f1fd324d6ac73e78d5abea
Nafis71 <nhtonmoy2@gmail.com>
2024-06-30 04:28:02 +0600

    animation bug fix

--- lib/widgets/profile_picture_widget.dart
+++ lib/widgets/profile_picture_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 import 'package:widget_and_text_animator/widget_and_text_animator.dart';
 
 import '../utils/assets.dart';
@@ -12,8 +13,8 @@ class ProfilePictureWidget extends StatelessWidget {
       incomingEffect: WidgetTransitionEffects.incomingSlideInFromRight(duration: const Duration(seconds: 4)),
       atRestEffect: WidgetRestingEffects.bounce(),
       child: Container(
-        height: MediaQuery.of(context).size.height * 0.45,
-        width: MediaQuery.of(context).size.width * 0.45,
+        height:  MediaQuery.of(context).size.height * 0.45,
+        width: ResponsiveBreakpoints.of(context).isDesktop ? MediaQuery.of(context).size.width * 0.45 : MediaQuery.of(context).size.width * 0.8,
         decoration: BoxDecoration(
           shape: BoxShape.circle,
           image: const DecorationImage(
